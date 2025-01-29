07d84b336c610a4a6dc472ea7ebce94eee69ae01
Asaduzzama-n <asad_uzzaman@outlook.com>
2025-01-29 20:55:58 +0600

    tracking socket added

--- src/helpers/socketHelper.ts
+++ src/helpers/socketHelper.ts
@@ -7,29 +7,25 @@ const socket = (io: Server) => {
   io.on('connection', (socket) => {
     logger.info(colors.blue('A user connected'));
 
-     // Handling live tracking
-     socket.on('liveTracking', async data => {
+   
+
+
+    socket.on('reservationTracking', async data => {
       try {
-        const { reservationId, latitude, longitude } = data;
-        if (!reservationId || latitude == null || longitude == null) {
+        const { professionalId, latitude, longitude } = data;
+        if (!professionalId || latitude == null || longitude == null) {
           return;
         }
 
-        // Check for delivery status
-        const updatedReservation = await stopTracking(reservationId, longitude, latitude);
-        if (!updatedReservation) {
-          return;
-        }
-        // Emit live tracking data
-        io.emit(`reservationTracking::${reservationId}`, {
+        socket.emit(`reservationTracking::${professionalId}`, {
           longitude: longitude,
           latitude: latitude,
         });
       } catch (error) {
         if (error instanceof Error) {
-          logger.error(`Error in liveTracking: ${error.message}`);
+          logger.error(`Error in reservationTracking: ${error.message}`);
         } else {
-          logger.error('Error in liveTracking: unknown error');
+          logger.error('Error in reservationTracking: unknown error');
         }
       }
     });
