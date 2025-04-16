612f2b8065b3ed897ece4e8d12b5d207e8bc6501
Asaduzzama-n <asad_uzzaman@outlook.com>
2025-04-16 14:10:11 +0600

    message updated

--- src/app/modules/message/message.validation.ts
+++ src/app/modules/message/message.validation.ts
@@ -1,7 +1,6 @@
 import { z } from 'zod';
 
 const sendMessageValidationSchema = z.object({
-  receiverId: z.string().min(24, 'receiverId must be a valid ObjectId'),
   message: z.string().min(1, 'Message is required').optional(),
   isRead: z.boolean().default(false),
   image: z.string().optional(),
