1a443b0ef7d9cf0d697c1ed3ac59821ef4580f88
Asaduzzama-n <asad_uzzaman@outlook.com>
2025-04-19 16:58:08 +0600

    done

--- src/app/modules/review/review.validation.ts
+++ src/app/modules/review/review.validation.ts
@@ -3,10 +3,7 @@ import { z } from 'zod';
 const createReviewZodSchema = z.object({
   body: z.object({
     review: z.string().min(1, 'Review is required.').optional(),
-    rating: z
-      .number()
-      .min(1, 'Rating must be at least 1.')
-      .max(5, 'Rating cannot exceed 5.'),
+    rating: z.number().max(5, 'Rating cannot exceed 5.'),
     reservation: z.string({ required_error: 'Reservation is required' }),
   }),
 });
